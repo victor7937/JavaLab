4071c55d2f044a2645d2164a915a533748d638e1
Victor Vyrostak <virostak@mail.ru>
2021-06-08 14:58:08 +0300

    add DataAccessExcepions Handler and logging

--- RestApiApp/persistence/src/main/java/com/epam/esm/exception/DataNotExistRepositoryException.java
+++ RestApiApp/persistence/src/main/java/com/epam/esm/exception/DataNotExistRepositoryException.java
@@ -1,5 +1,6 @@
 package com.epam.esm.exception;
 
+
 public class DataNotExistRepositoryException extends RepositoryException {
 
     private static final long serialVersionUID = 8704791513196513985L;
--- RestApiApp/web/src/main/java/com/epam/esm/handler/GlobalExceptionHandler.java
+++ RestApiApp/web/src/main/java/com/epam/esm/handler/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.epam.esm.handler;
 
 import com.epam.esm.message.ResponseExceptionMessage;
+
+import org.apache.log4j.Logger;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,23 +11,37 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
 
 
+
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final String EXCEPTION_CAUGHT_MSG = "Exception was caught in GlobalExceptionHandler:";
+    Logger logger = Logger.getLogger(GlobalExceptionHandler.class);
+    private static final String DATABASE_ERROR_MSG = "Database Error";
+    private static final String SERVER_ERROR_MSG = "Server Error";
+
     @ExceptionHandler(value = ResponseStatusException.class)
     public ResponseEntity<ResponseExceptionMessage> handleException(ResponseStatusException e){
+        logger.error(EXCEPTION_CAUGHT_MSG, e);
         return new ResponseEntity<>(new ResponseExceptionMessage(e.getStatus(), e.getReason()), e.getStatus());
     }
 
     @ExceptionHandler(value = NumberFormatException.class)
     public ResponseEntity<ResponseExceptionMessage> handleException(NumberFormatException e){
+        logger.error(EXCEPTION_CAUGHT_MSG, e);
         return new ResponseEntity<>(new ResponseExceptionMessage(HttpStatus.BAD_REQUEST, e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
-    //Было бы интересно давать разные статус коды для разных исключений, а не просто 500
-    //Пока не знаю как так сделать(может и не надо)
-    @ExceptionHandler(value = RuntimeException.class)
-    public ResponseEntity<ResponseExceptionMessage> handleException(RuntimeException e){
-        return new ResponseEntity<>(new ResponseExceptionMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(value = DataAccessException.class)
+    public ResponseEntity<ResponseExceptionMessage> handleException(DataAccessException e){
+        logger.error(EXCEPTION_CAUGHT_MSG, e);
+        return new ResponseEntity<>(new ResponseExceptionMessage(HttpStatus.INTERNAL_SERVER_ERROR, DATABASE_ERROR_MSG), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ResponseExceptionMessage> handleException(Exception e){
+        logger.error(EXCEPTION_CAUGHT_MSG, e);
+        return new ResponseEntity<>(new ResponseExceptionMessage(HttpStatus.INTERNAL_SERVER_ERROR, SERVER_ERROR_MSG), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
